b22ad25104e4bb2b6046f0544ee2e7675e2429a2
Kevin Rivera <kevinrev26@gmail.com>
2019-03-13 18:08:14 -0600

    Canvas for labels added

--- resources/util.js
+++ resources/util.js
@@ -89,6 +89,10 @@ var robox = (function() {
     preserveDrawingBuffer: true,
     premultipliedAlpha: false
   });
+  //rendererAxis.gammaOutput = true;
+  //rendererAxis.gammaFactor = 2.2;
+  //rendererAxis.toneMappingExposure = 1.0;
+  rendererAxis.setPixelRatio(window.devicePixelRatio);
   /*rendererAxis.setSize(80, 80);
   rendererAxis.setClearColor(0xd4d4bf, 1);*/
 
@@ -106,6 +110,12 @@ var robox = (function() {
   cameraAxis.position.set(0, 0, cameraDistance);
   // cubes as buttons
   addCubesToAxisScene(sceneAxis);
+
+  //Labels?
+  /*const front = makeTextSprite("FRENTE");
+  front.position.z += 1;
+
+  sceneAxis.add(front);*/
   const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
   // ambientLight.position.set(0, 1, 0);
 
@@ -289,7 +299,12 @@ function createGizmoContainer() {
     gizmoContainer.clientWidth,
     gizmoContainer.clientHeight
   );
-  gizmoContainer.appendChild(robox.getAxisRenderer().domElement);
+  const gizmoRenderer = robox.getAxisRenderer();
+  gizmoRenderer.setSize(
+    gizmoContainer.clientWidth,
+    gizmoContainer.clientHeight
+  );
+  gizmoContainer.appendChild(gizmoRenderer.domElement);
 }
 
 function executeCommand(command) {
@@ -328,6 +343,111 @@ function executeCommand(command) {
     default:
       break;
   }
+}
+
+function makeTextSprite(message, parameters) {
+  if (parameters === undefined) parameters = {};
+
+  var fontface = parameters.hasOwnProperty("fontface")
+    ? parameters["fontface"]
+    : "Arial";
+
+  var fontsize = parameters.hasOwnProperty("fontsize")
+    ? parameters["fontsize"]
+    : 18;
+
+  var borderThickness = parameters.hasOwnProperty("borderThickness")
+    ? parameters["borderThickness"]
+    : 4;
+
+  var borderColor = parameters.hasOwnProperty("borderColor")
+    ? parameters["borderColor"]
+    : { r: 0, g: 0, b: 0, a: 1.0 };
+
+  var backgroundColor = parameters.hasOwnProperty("backgroundColor")
+    ? parameters["backgroundColor"]
+    : { r: 255, g: 255, b: 255, a: 1.0 };
+
+  //var spriteAlignment = parameters.hasOwnProperty("alignment") ?
+  //	parameters["alignment"] : THREE.SpriteAlignment.topLeft;
+
+  var canvas = document.createElement("canvas");
+  var context = canvas.getContext("2d");
+  context.font = "Bold " + fontsize + "px " + fontface;
+
+  // get size data (height depends only on font size)
+  var metrics = context.measureText(message);
+  var textWidth = metrics.width;
+
+  // background color
+  context.fillStyle =
+    "rgba(" +
+    backgroundColor.r +
+    "," +
+    backgroundColor.g +
+    "," +
+    backgroundColor.b +
+    "," +
+    backgroundColor.a +
+    ")";
+  // border color
+  context.strokeStyle =
+    "rgba(" +
+    borderColor.r +
+    "," +
+    borderColor.g +
+    "," +
+    borderColor.b +
+    "," +
+    borderColor.a +
+    ")";
+
+  context.lineWidth = borderThickness;
+  // TODO background fill.
+  roundRect(
+    context,
+    borderThickness / 2,
+    borderThickness / 2,
+    textWidth + borderThickness,
+    fontsize * 1.4 + borderThickness,
+    6
+  );
+  // 1.4 is extra height factor for text below baseline: g,j,p,q.
+
+  // text color
+  context.fillStyle = "rgba(0, 0, 0, 1.0)";
+
+  context.fillText(message, borderThickness, fontsize + borderThickness);
+
+  // canvas contents will be used for a texture
+  var texture = new THREE.Texture(canvas);
+  texture.needsUpdate = true;
+
+  var spriteMaterial = new THREE.SpriteMaterial({
+    map: texture,
+    useScreenCoordinates: false
+  });
+  var sprite = new THREE.Sprite(spriteMaterial);
+  //TODO Set Scale
+  sprite.scale.set(40, 30, 1.5);
+  return sprite;
+}
+
+// function for drawing rounded rectangles
+function roundRect(ctx, x, y, w, h, r) {
+  ctx.beginPath();
+  ctx.moveTo(x + r, y);
+  ctx.lineTo(x + w - r, y);
+  ctx.quadraticCurveTo(x + w, y, x + w, y + r);
+  ctx.lineTo(x + w, y + h - r);
+  ctx.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
+  ctx.lineTo(x + r, y + h);
+  ctx.quadraticCurveTo(x, y + h, x, y + h - r);
+  ctx.lineTo(x, y + r);
+  ctx.quadraticCurveTo(x, y, x + r, y);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
 }
 
 /**
