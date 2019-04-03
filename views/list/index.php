9af92d21c3785d59f99e1eeb7f6e467a27c4ec2b
Kevin Rivera <kevinrev26@gmail.com>
2019-04-03 17:17:25 -0600

    Lessons list views updated

--- views/list/index.php
+++ views/list/index.php
@@ -7,34 +7,42 @@ use Yii;
 ?>
 
 <div class="panel panel-default">
-<div class="panel-body">
+    <div class="panel-body">
         <div class="row">
             <?php
             $cookies = Yii::$app->request->cookies;
             $parts = preg_split('/-+/', $contentContainer->name);
             $grade = $parts[0][strlen($parts[0]) - 2] . $parts[0][strlen($parts[0]) - 1];
-            $models = ConfigureForm::getModelsByGrade(strtoupper($grade));
-            $counter = 0;
-            echo "<ul>";
-            foreach ($models as $key => $value) {
-                $routes = preg_split('/\//', $value);
-                $msg = 'Sin completar';
-                $model = preg_split('/\./', $routes[sizeof($routes) - 1])[0];
-                if (isset($_COOKIE[$model])) {
-                    $msg = "Visto!";
-                }
-                echo "<li> Modelo: " . $model . " " .
-                    Button::primary(Yii::t('DrawioModule.base', 'Ver'))->link($contentContainer->createUrl("//drawio/page", ['route' => $value])) .
-                    " " . $msg .
-                    " </li>";
+            $models = ConfigureForm::getModelsByGrade(strtoupper($grade)); ?>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <td scope="col">Número de lección</td>
+                        <td scope="col"></td>
+                        <td scope="col">Estado</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($models as $key => $value) : ?>
+                    <tr>
+                        <?php 
+                        $routes = preg_split('/\//', $value);
+                        $msg = 'Sin completar';
+                        $model = preg_split('/\./', $routes[sizeof($routes) - 1])[0];
+                        if (isset($_COOKIE[$model])) {
+                            $msg = "Visto";
+                        }
+                        ?>
+                        <td><?php echo $model ?></td>
+                        <td><?php echo Button::primary(Yii::t('DrawioModule.base', 'Ver'))->link($contentContainer->createUrl("//drawio/page", ['route' => $value])) ?></td>
+                        <td><?php echo $msg ?> </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
 
 
 
-            }
-            echo "</ul>"
-            ?>
-            
-            
         </div>
     </div>
-</div>
+</div> 
